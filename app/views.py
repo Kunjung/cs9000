6f2a5602a8296c175fbd9e51add1da29d0ee2cdc
Kunjung Sherpa <kunjungsherpa@gmail.com>
2017-08-21 08:30:13 +0545

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, url_for, request, session
+from flask import render_template, redirect, url_for, request, session, jsonify
 
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField, PasswordField, BooleanField, IntegerField, FloatField
@@ -150,3 +150,28 @@ def rate(movie_id):
 def logout():
 	logout_user()
 	return redirect(url_for('home'))
+
+
+
+#########################
+### JSON API STUFF #####
+#########################
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+@app.route('/api/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
